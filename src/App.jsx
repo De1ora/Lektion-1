9d04f07e6f9c9d1dd44eb6c15be8e92a6bb39307
Lisa <lisafalth05@gmail.com>
2025-08-24 12:38:43 +0200

    Grundläggande övningar uppgift 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-function LabeledInput() {
-  return <>
-  <label>Label</label>
-  <input />
-  </>
-}
-
-function MyComponent() {
-  return <p>Hello from a new component!</p>
-}
+  // 1. Skapa en egen komponent
+  function MyComponent() {
+    return <p>Hello from a new component!</p>
+  }
 
   return (
     <>
-    <LabeledInput/>
-    <MyComponent></MyComponent>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MyComponent />
     </>
   )
 }
