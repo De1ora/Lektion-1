b432143c7147da93d00c73aca76732188fefd578
Lisa <lisafalth05@gmail.com>
2025-08-24 12:43:37 +0200

    Grundläggande övningar uppgift 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css'
 
-function App() {
-
   // 1. Skapa en egen komponent
   function MyComponent() {
     return <p>Hello from a new component!</p>
   }
 
+  // 2. Skapa en egen komponent
+  function LoveReact() {
+    return <p>I love React!</p>
+  }
+
+
+function App() {
   return (
     <>
       <MyComponent />
+      <LoveReact />
     </>
   )
 }
