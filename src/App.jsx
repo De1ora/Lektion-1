5184ec17a43320dec2214b51b2df7b18f30c2055
Lisa <lisafalth05@gmail.com>
2025-08-24 12:58:20 +0200

    Grundläggande övningar uppgift 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css'
 
-  // 1. Skapa en egen komponent
-  function MyComponent() {
-    return <p>Hello from a new component!</p>
-  }
+// 1. Skapa en egen komponent
+function MyComponent() {
+  return ( // Parentesen tillkommer pga radbrytning. Vanliga JS-kommentarer fungerar utanför JSX.
+    <> {/* Eftersom MyComponent nu retunerar två element, måste de wrappas som ett element med <> */}
+    <p>Hello from a new component!</p>
+    <SubComponent />
+  </>
+  )
+}
 
-  // 2. Skapa en egen komponent
-  function LoveReact() {
-    return <p>I love React!</p>
-  }
+// 2. Skapa en egen komponent
+function LoveReact() {
+  return <p>I love React!</p>
+}
+
+function SubComponent() {
+  return <p>This is a sub-component</p>
+}
 
 
 function App() {
