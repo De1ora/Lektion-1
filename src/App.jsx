73126fc6e9cf8c9ba87dec195e566881f035be41
Lisa <lisafalth05@gmail.com>
2025-08-27 09:33:22 +0200

    Ett exempel med lifecycles

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,23 @@
 import './App.css'
+import { useEffect, useState } from 'react';
 
-// 9. Struktur för webbsida 
-// Skapa komponenter som bygger upp strukturen för en webbsida. Sidan ska innehålla: header, 
-// navbar, sidebar, main-content och footer. Komponenterna behöver inte ha något innehåll men 
-// ska länkas i App komponenten.
+function LifecycleExample() {
+  useEffect(() => {
+    console.log("Opened");
+  }, []);
 
-function Header() {
-  return <header>Header</header>
-}
-
-function Navbar() {
-  return <nav>Navbar</nav>
-}
-
-function Sidebar() {
-  return <aside>Sidebar</aside>
-}
-
-function MainContent() {
-  return <main>Main Content</main>
-}
-
-function Footer() {
-  return <footer>Footer</footer>
+  return <div>Showing</div>;
 }
 
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <Header />
-      <Navbar />
-      <div className="content-wrapper">
-        <Sidebar />
-        <MainContent />
-      </div>
-      <Footer />
+      {open ? <LifecycleExample /> : null}
+      <button onClick={() => setOpen(!open)}>Show</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
